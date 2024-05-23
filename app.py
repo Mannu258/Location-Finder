9ff32dfe32bafd545341b40c7197f1d005c949fe
Your Name <you@example.com>
2024-05-23 12:58:02 +0530

    added login panel and added 2 root first is administrator and sacond is account

--- app.py
+++ app.py
@@ -16,6 +16,13 @@ class Location(db.Model):
         return datetime.utcnow() + timedelta(hours=5, minutes=30)
     created_at = db.Column(db.DateTime, default=get_ist_now)
 
+class Crediantials(db.Model):
+    ID = db.Column(db.Integer,primary_key=True)
+    username = db.Column(db.String(200),nullable=False)
+    password = db.Column(db.String(200),nullable=False)
+    def __repr__(self) -> str:
+        return f"{self.username}"
+
 
 @app.route("/", methods=['GET'])
 def index():
@@ -30,10 +37,40 @@ def index():
         db.session.commit()
     return render_template('index.html')
 
-@app.route("/secretadmin", methods=['GET'])
-def details():
-    location = Location.query.order_by(Location.ID.desc()).all()
-    return render_template('details.html',location=location)
+
+@app.route("/administrator",methods=['POST','GET'])
+def Admin():
+    if request.method == "POST":
+        username = request.form['Username']
+        password = request.form['password']
+        admi = Crediantials.query.filter_by(username=username,password=password).first()
+        if admi:
+            location = Location.query.order_by(Location.ID.desc()).all()
+            return render_template('details.html',location=location)
+
+        else:
+            return render_template('login.html')
+
+    return render_template('login.html')
+
+
+
+@app.route("/account",methods=['POST','GET'])
+def Account():
+    if request.method == "POST":
+        username = request.form['Username']
+        password = request.form['password']
+        existing_user = Crediantials.query.filter_by(username=username).first()
+        if existing_user:
+            return "Account already exists. Please log in."
+        
+        if username:
+            location = Crediantials(username = username,password=password)
+            db.session.add(location)
+            db.session.commit()
+            return "Account Created Successfully"
+    return render_template('account.html')
+
 
 if __name__ == '__main__':
    app.run(debug=True ,port=8000)
